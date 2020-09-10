df17b772a1153b1abb591af482c82ce93d2b8c0f
faranak Sabet <faranaksabet@yahoo.com>
2020-09-10 11:16:09 -0700

    schema added

---
+++ schema.sql
@@ -0,0 +1,40 @@
+DROP DATABASE IF EXISTS employeeDB;
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE employee
+(
+    id INT
+    AUTO_INCREMENT NOT NULL,
+     first_name VARCHAR
+    (30) NOT NULL,
+     last_name VARCHAR
+    (30) NOT NULL,
+     role_id INT NOT NULL,
+     manager_id INT NULL,
+     PRIMARY KEY
+    (id)
+    );
+
+    CREATE TABLE department
+    (
+        id INT NOT NULL
+        AUTO_INCREMENT,
+    name VARCHAR
+        (30) NOT NULL,
+    primary key
+        (id)
+    );
+
+        CREATE TABLE role
+        (
+            id INT NOT NULL
+            AUTO_INCREMENT,
+    title VARCHAR
+            (30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY
+            (id)
+    );

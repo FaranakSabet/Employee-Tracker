ba6f5ae4b9950babffd67754e40e86e5961a014f
faranak Sabet <faranaksabet@yahoo.com>
2020-09-16 10:55:57 -0700

    updated Schema

--- schema.sql
+++ schema.sql
@@ -1,40 +1,32 @@
-DROP DATABASE IF EXISTS employeeDB;
-CREATE DATABASE employeeDB;
+DROP DATABASE IF EXISTS employeesDB;
+CREATE database employeesDB;
 
-USE employeeDB;
+USE employeesDB;
 
 CREATE TABLE employee
 (
-    id INT
-    AUTO_INCREMENT NOT NULL,
-     first_name VARCHAR
+    id INT NOT NULL
+    AUTO_INCREMENT,
+    first_name VARCHAR
     (30) NOT NULL,
-     last_name VARCHAR
+    last_name VARCHAR
     (30) NOT NULL,
-     role_id INT NOT NULL,
-     manager_id INT NULL,
-     PRIMARY KEY
-    (id)
-    );
-
-    CREATE TABLE department
-    (
-        id INT NOT NULL
-        AUTO_INCREMENT,
-    name VARCHAR
-        (30) NOT NULL,
-    primary key
-        (id)
-    );
-
-        CREATE TABLE role
-        (
-            id INT NOT NULL
-            AUTO_INCREMENT,
-    title VARCHAR
-            (30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
     PRIMARY KEY
-            (id)
-    );
+    (id)
+);
+
+-- CREATE TABLE role (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     title VARCHAR(30) NOT NULL,
+--     salary DECIMAL NOT NULL,
+--     department_id INT NOT NULL,
+--     PRIMARY KEY (id)
+-- );
+
+-- CREATE TABLE department (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     name VARCHAR(30) NOT NULL,
+--     PRIMARY KEY (id)
+-- );

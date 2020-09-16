239d19cc45f7646fa09b339c8713ca74f2f3f6ac
faranak Sabet <faranaksabet@yahoo.com>
2020-09-16 11:13:28 -0700

    Finished schema.sql

--- schema.sql
+++ schema.sql
@@ -17,16 +17,24 @@ CREATE TABLE employee
     (id)
 );
 
--- CREATE TABLE role (
---     id INT NOT NULL AUTO_INCREMENT,
---     title VARCHAR(30) NOT NULL,
---     salary DECIMAL NOT NULL,
---     department_id INT NOT NULL,
---     PRIMARY KEY (id)
--- );
+    CREATE TABLE role
+    (
+        id INT NOT NULL
+        AUTO_INCREMENT,
+    title VARCHAR
+        (30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY
+        (id)
+);
 
--- CREATE TABLE department (
---     id INT NOT NULL AUTO_INCREMENT,
---     name VARCHAR(30) NOT NULL,
---     PRIMARY KEY (id)
--- );
+        CREATE TABLE department
+        (
+            id INT NOT NULL
+            AUTO_INCREMENT,
+    name VARCHAR
+            (30) NOT NULL,
+    PRIMARY KEY
+            (id)
+);
